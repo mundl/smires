da0deaf835b29e3cf96080ed72f324fe055f1fd7
Tobias Gauster <t.gauster@koop-wasser.at>
2017-02-10 16:43:49 +0100

    adding title and threshold to plot

--- R/plot.R
+++ R/plot.R
@@ -1,28 +1,29 @@
 plot_events <- function(x, size = 5)
 {
   require(ggplot2)
-  e <- events
   .day <- function(x) as.numeric(format(x, "%j"))
 
   # we need the complete sequence of years as factor levels
   # otherwise this years would be omitted in the plot
-  year <- format(e$start, "%Y")
+  year <- format(x$start, "%Y")
   rng <- range(as.numeric(year))
-  e$year <- factor(year, levels = as.character(seq(rng[1], rng[2])))
+  x$year <- factor(year, levels = as.character(seq(rng[1], rng[2])))
 
   # recode stard and end as day of the year (julian day)
   # slightly incorrect for leap years...
-  e$start <- .day(e$start)
-  e$end <- .day(e$end)
+  x$start <- .day(x$start)
+  x$end <- .day(x$end)
 
   # position labels months
   breaks <- c(1, 32, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 367)
   at <- (head(breaks, -1) + tail(breaks, -1) - 2) / 2
 
-  ggplot(e, aes(col = state)) +
+  ggplot(x, aes(col = state)) +
     geom_vline(xintercept = breaks, col = "white") +
     geom_segment(aes(x = start, xend = end + 1, y = year, yend = year),
                  size = size) +
+    labs(title = paste0("Stream-Flow Permanence (threshold = ",
+                        attr(x, "threshold") ,")")) +
     scale_x_continuous(breaks = at, labels = month.abb, expand = c(0, 0)) +
     scale_y_discrete(expand = c(0, 0.5), drop = F) +
     theme(panel.grid = element_blank(),
