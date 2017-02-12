2607cb422eed623011ab15563dc402e63562dbdc
Tobias Gauster <t.gauster@koop-wasser.at>
2017-02-12 21:14:17 +0100

    first draft presentation

---
+++ dev/metrics.R
@@ -0,0 +1,14 @@
+library(smires)
+
+
+find_events(balder, period = "year", threshold = 0.05) %>%
+  summarise_at(vars(duration), funs(max)) %>%
+  group_by(state) %>%
+  summarise_at(vars(-period, -state), funs(mean))
+
+
+plot_events(find_events(ampneyBrook, period = "year", threshold = 0.01))
+
+m <- find_events(ampneyBrook, period = "month", threshold = 0.05) %>%
+  summarise_at(vars(duration), funs(max, mean))
+plot_period(m, type = "ts")
