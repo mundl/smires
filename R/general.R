19970c3c1637594f4518385f48f1d6b2eaab601e
Tobias Gauster <t.gauster@koop-wasser.at>
2017-02-13 20:10:05 +0100

    is.intermittent() doesn't check for "years"

--- R/general.R
+++ R/general.R
@@ -1,29 +1,18 @@
-is.intermittent <- function(x, ndays = 5, nyears = 1,
-                            consecutive = c("none", "days", "years"),
-                            threshold = 0.001)
+is.intermittent <- function(x, ndays = 5, consecutive = TRUE, threshold = 0.001,
+                            period = "year")
+  # todo: find_event() must support water year.
 {
 
-  consecutive <- match.arg(consecutive, several.ok = TRUE)
-  e <- find_events(x, threshold = threshold, na.rm = FALSE, period = "year",
+  e <- find_events(x, threshold = threshold, na.rm = FALSE, period = period,
                    warn = FALSE)
   e <- e[e$state == "dry" & !is.na(e$event), ]
-  spell <- filter(e, duration >= ndays)
 
-  intermittent <-  if (any(consecutive == "none"))
-    {
-    sum(e$duration, na.rm = TRUE) > ndays & length(unique(spell$period)) >= nyears
-  } else if(all(consecutive == "days"))
+  days <- if(consecutive)
   {
-    length(unique(spell$period)) >= nyears
-  } else if(all(consecutive == "years"))
-  {
-    rl <- rle(diff(as.numeric(as.character(unique(e$period)))))
-    any(rl$lengths[rl$values == 1] >= nyears - 1)
-  } else if(all(c("years", "days") %in% consecutive))
-  {
-    rl <- rle(diff(as.numeric(as.character(unique(spell$period)))))
-    any(rl$lengths[rl$values == 1] >= nyears - 1)
+    max(e$duration, na.rm = TRUE)
+  } else {
+    sum(e$duration, na.rm = TRUE)
   }
 
-  return(intermittent)
+  return(days >= ndays)
 }
