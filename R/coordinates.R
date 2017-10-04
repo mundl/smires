33ae6f1ab9a4f83d681491c95e076809dc1d4ae4
Tobias Gauster <t.gauster@boku.ac.at>
2017-10-04 17:36:47 +0200

    transform_crs is vectorized over epsg

--- R/coordinates.R
+++ R/coordinates.R
@@ -1,20 +1,39 @@
 transform_crs <- function(x, y, from, to)
 {
-  require(sf)
-  xy <- data.frame(x, y)
+  len <- lengths(list(from = from, to = to))
 
-  # we cannot transform NA values
-  transformed <- matrix(NA, nrow = nrow(xy), ncol = 2)
-  mask <- complete.cases(xy)
-  xy <- xy[mask, ]
+  if(any(len > 1)) {
+    if(!all(len %in% c(1, length(x)))) {
+      stop("Argument 'from' and 'to' must be of length 1 or ", length(x), ".")
+    }
+    data <- data.frame(x = x, y = y, from = from, to = to, row.names = seq_along(x))
 
-  if(nrow(xy) > 0) {
-    p1 <- st_as_sf(xy, coords = c("x", "y"), crs = from)
-    p2 <- st_transform(p1, crs = to)
-    transformed[mask, ] <- do.call(rbind, st_geometry(p2))
+    g <- data[, c("from", "to")]
+
+    z <- by(data, g, function(x) data.frame(transform_crs(x$x, x$y,
+                                                          from = unique(x$from),
+                                                          to = unique(x$to)),
+                                            id = as.numeric(rownames(x))))
+    z <- do.call(rbind, z)
+    z <- as.matrix(z[order(z$id), 1:2])
+    return(unname(z))
+  } else {
+    require(sf)
+    xy <- data.frame(x, y)
+
+    # we cannot transform NA values
+    transformed <- matrix(NA, nrow = nrow(xy), ncol = 2)
+    mask <- complete.cases(xy)
+    xy <- xy[mask, ]
+
+    if(nrow(xy) > 0) {
+      p1 <- st_as_sf(xy, coords = c("x", "y"), crs = from)
+      p2 <- st_transform(p1, crs = to)
+      transformed[mask, ] <- do.call(rbind, st_geometry(p2))
+    }
+
+    return(transformed)
   }
-
-  return(transformed)
 }
 
 has_coordinates <- function(x) {
